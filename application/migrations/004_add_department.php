e0494365825112175ca2094be874ca77288c4096
vishaly99 <81356194+vishaly99@users.noreply.github.com>
2021-03-25 17:52:44 +0530

    table department crated

---
+++ application/migrations/004_add_department.php
@@ -0,0 +1,58 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * 
+ */
+	class Migration_Add_department extends CI_Migration {
+
+        public function up()
+        {
+                $this->dbforge->add_field(array(
+                        'id' => array(
+                                'type' => 'INT',
+                                'auto_increment' => TRUE
+                        ),
+                        'departmentname' => array(
+                                'type' => 'VARCHAR',
+                                'constraint' => '100',
+                        ),
+                        'departmentcode' => array(
+                                'type' => 'VARCHAR',
+                                'constraint' => '10',
+                                 'unique'=>TRUE
+                        ),
+                        'clientid' => array(
+                                'type' => 'VARCHAR',
+                                'constraint' => '10',
+                        ),
+                        'branchid' => array(
+                                'type' => 'VARCHAR',
+                                'constraint' => '10',
+                        ),
+                        'ipaddress' => array(
+                                'type' => 'VARCHAR',
+                                'constraint' => '20',
+                        ),
+                        'status' => array(
+                                'type' => 'VARCHAR',
+                                'constraint' => '10',
+                        ),
+                        'createddate' => array(
+                                'type' => 'DATETIME',
+                                'default'=>date('Y-m-d H:i:s'),
+                        ),
+                        
+                        
+                ));     
+                $this->dbforge->add_key('id', TRUE);
+                $this->dbforge->create_table('department');
+        }
+
+        public function down()
+        {
+                $this->dbforge->drop_table('department');
+        }       
+}
+
+?>
