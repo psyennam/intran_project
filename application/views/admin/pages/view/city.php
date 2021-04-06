b16968cfd6532b0ad9b521d20ed3af00e595fa92
vishaly99 <81356194+vishaly99@users.noreply.github.com>
2021-04-06 19:25:03 +0530

    changes role and state

--- application/views/admin/pages/view/city.php
+++ application/views/admin/pages/view/city.php
@@ -69,7 +69,7 @@
             <button type="button" class="close" data-dismiss="modal">&times;</button> 
           </div>
           <!-- Form 1 Start  -->
-          <form action="<?= base_url('Userpanel/cityinsert');?>" id="formone" class="form-group" method="post">
+          <form action="<?= base_url('Admin/cityinsert');?>" id="formone" class="form-group" method="post">
             <div class="modal-body">
               <div class="row">
                 <div class="col-sm-12 col-md-12 col-lg-4">
@@ -128,7 +128,7 @@
             <h4 class="modal-title">Pincode</h4>  
           </div>
           <!-- Form 2 Start  -->
-          <form action="/intran_project/Userpanel/pincodeinsert" id="formone" class="form-group" method="post" enctype="multipart/form-data">   
+          <form action="<?php echo base_url('Admin/pincodeinsert');?>" id="formone" class="form-group" method="post" enctype="multipart/form-data">   
             <div class="modal-body">
               <input type="hidden" name="city" value="" id="h_city_code">
               <div class="row">
--- application/views/admin/pages/view/state.php
+++ application/views/admin/pages/view/state.php
@@ -1,6 +1,3 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?>
  <!-- Content Wrapper. Contains page content -->
   		<!-- Form Start  -->
         
@@ -22,111 +19,33 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <div class="col-xs-12">
           <div class="box">
             <div class="box-header">
-              <!-- Form Start  -->
-        <form action="<?php echo base_url('Admin/stateinsert');?>" id="formone" class="form-group" method="post" enctype="multipart/form-data">   
-              	<button type="button" class="btn btn-success" data-toggle="modal" data-target="#mymodel">Add</button>
-          		<!-- Modal Start  -->
-              	<div class="modal fade" id="mymodel">
-              		<div class="modal-dialog modal-dialog-centered">
-              			<div class="modal-content">
-              				<div class="modal-header">
-              					<h3 class="text-center text-primary">State</h3>	
-              					<button type="button" class="close" data-dismiss="modal">&times;</button>	
-              				</div>
-              				<div class="modal-body">
-                            <div class="row">
-                              <div class="col-sm-12 col-md-12 col-lg-4">
-                                  <label>Country</label>
-                                  <select class="form-control" name="countrycombo">
-                                  <?php 
-                                  foreach ($countrydetails as $row) {
-                                  ?>
-                                      <option value="<?php echo $row->country_code ?>"><?php echo $row->country; ?>
-                                      </option>
-                                  <?php
-                                } ?>
-                                  </select>
-                              </div>
-  							              <div class="col-sm-12 col-md-4 col-lg-4">
-  							                <label>State Name</label>
-  							                <input type="text" class="form-control" placeholder="Enter State Name" name="StateName">
-  							              </div>
-  							              <div class="col-sm-12 col-md-12 col-lg-4">
-  							                <label>Organization Id</label>
-  							                <input type="text" class="form-control" name="ClientId" value="<?php echo $this->session->userdata('org_code');?>" disabled>
-  							              </div>
-  							            </div>
-							       
-              				</div>
-              				<div class="modal-footer">
-              					<div class="row">
-							              <div class="col-md-12 text-center" style="margin-top: 10px;">
-							               <button type="submit" class="btn btn-primary">Submit</button>
-							              </div>
-							    </div>
-              				</div>
-              			</div>
-              		</div>
-              	</div>
-            	<!-- End Modal  -->
-        </form>
-         <!-- End Form Start  -->
+              <button type="button" class="btn btn-success" data-toggle="modal" data-target="#mymodel">Add</button>
             </div>
             <!-- /.box-header -->
             <div class="box-body" style="overflow-x:auto;">
               <table id="example2" class="table table-bordered table-hover">
                 <thead>
                 <tr>
-          <th>ID</th>
-					<th>Country Code</th>
-          <th>State Code</th>
-					<th>State Name</th>
-					<th>Organization Code</th>
-					<th>Created_at</th>
-					<th>Status</th>
-					<th>IpAddress</th>
-					<th>Branch Code</th>
-<!-- 					<th>Update</th>
-					<th>Delete</th> -->
-			    </tr>
+                <th>ID</th>
+      					<th>Country Code</th>
+                <th>State Code</th>
+      					<th>State Name</th>
+      					<th>Created_at</th>
+      					<th>Status</th>
+      			    </tr>
                 </thead>
-               <tbody>
-              <?php
-	foreach ($statedetails as $key) {
-	?>
-	<tr>
-		<td><?php echo $key->id;?></td>
-		<td><?php echo $key->country_code; ?></td>	
-    <td><?php echo $key->state_code; ?></td>  
-		<td><?php echo $key->state; ?></td>	
-		<td><?php echo $key->org_code;?></td>	
-		<td><?php echo $key->created_at; ?></td>	
-		<td><?php echo $key->status; ?></td>	
-		<td><?php echo $key->ip_address; ?></td>	
-		<td><?php echo $key->branch_code; ?></td>	
-		<!-- <td><a href="updaterole?id=<?php echo $key->role_code;?>"><button type="button" class="btn btn-block btn-primary" data-toggle="modal" data-target="#updatemodel">UPDATE</button></a></td>
-		<td><a href="deleterole?id=<?php echo $key->role_code;?>"><button type="button" class="btn btn-block btn-primary">DELETE</button></a></td>	 -->				
-	</tr>	
-	<?php
-	}
-		?>
-                
-                </tbody>
-                <tfoot>
-                <tr>
-					<th>ID</th>
-          <th>Country Code</th>
-          <th>State Code</th>
-          <th>State Name</th>
-          <th>Organization Code</th>
-          <th>Created_at</th>
-          <th>Status</th>
-          <th>IpAddress</th>
-          <th>Branch Code</th>
-			<!-- 		<th>Update</th>
-					<th>Delete</th> -->
-			    </tr>
-                </tfoot>
+                  <tbody>
+                    <?php foreach ($statedetails as $key) { ?>
+                      	<tr>
+                      		<td><?php echo $key->id;?></td>
+                      		<td><?php echo $key->country_code; ?></td>	
+                          <td><?php echo $key->state_code; ?></td>  
+                      		<td><?php echo $key->state; ?></td>	
+                      		<td><?php echo __date_format($key->created_at, 'ddmmyyyy'); ?></td>	
+                      		<<td><?php echo is_status($key->status); ?></td>				
+                      	</tr>	
+                      	<?php } ?>     
+                  </tbody>
               </table>
             </div>
             <!-- /.box-body -->
@@ -139,8 +58,50 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     </section>
     <!-- /.content -->
 
-  <!-- /.content-wrapper -->
-
+<!-- Modal Start  -->
+  <div class="modal fade" id="mymodel">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h3 class="text-center text-primary">State</h3> 
+          <button type="button" class="close" data-dismiss="modal">&times;</button> 
+        </div>
+            <!-- Form Start  -->
+            <form action="<?php echo base_url('Admin/stateinsert');?>" id="formone" class="form-group" method="post">   
+              <div class="modal-body">
+                    <div class="row">
+                      <div class="col-sm-12 col-md-12 col-lg-4">
+                          <label>Country</label>
+                          <select class="form-control" name="countrycombo">
+                          <?php foreach ($countrydetails as $row) { ?>
+                              <option value="<?php echo $row->country_code ?>"><?php echo $row->country; ?>
+                              </option>
+                          <?php } ?>
+                          </select>
+                      </div>
+                      <div class="col-sm-12 col-md-4 col-lg-4">
+                        <label>State Name</label>
+                        <input type="text" class="form-control" placeholder="Enter State Name" name="StateName">
+                      </div>
+                      <div class="col-sm-12 col-md-12 col-lg-4">
+                        <label>Organization Id</label>
+                        <input type="text" class="form-control" name="ClientId" value="<?php echo $this->session->userdata('org_code');?>" disabled>
+                      </div>
+                    </div>
+              </div>
+                <div class="modal-footer">
+                    <div class="row">
+                        <div class="col-md-12 text-center" style="margin-top: 10px;">
+                         <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+            <!-- End Form Start  -->
+      </div>
+    </div>
+  </div>
+<!-- End Modal  -->
 <!-- page script -->
 <script>
   $(function () {
