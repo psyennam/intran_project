b8136e6f12ad926ea5781533442dfe76b33aac75
dhruvil2811 <81356185+dhruvil2811@users.noreply.github.com>
2021-04-06 20:27:03 +0530

    dasignation done

--- application/views/admin/pages/view/designation.php
+++ application/views/admin/pages/view/designation.php
@@ -1,135 +1,87 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?>
- <!-- Content Wrapper. Contains page content -->
-  		
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-        Designation Data
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i>Home</a></li>
-        <li><a href="#">Tables</a></li>
-        <li class="active">Data tables</li>
-      </ol>
-    </section>
-
-    <!-- Main content -->
+<section class="content-header">
+  <h1>Designation Data</h1>
+</section>
+  <!-- Main content -->
     <section class="content">
       <div class="row">
         <div class="col-xs-12">
           <div class="box">
             <div class="box-header">
-              <!-- Form Start  -->
-        <form action="<?php base_url('Admin/designationinsert');?>" id="formone" class="form-group" method="post" enctype="multipart/form-data">   
               	<button type="button" class="btn btn-success" data-toggle="modal" data-target="#mymodel">Add</button>
-          		<!-- Modal Start  -->
-              	<div class="modal fade" id="mymodel">
-              		<div class="modal-dialog modal-dialog-centered">
-              			<div class="modal-content">
-              				<div class="modal-header">
-              					<h3 class="text-center text-primary">Role</h3>	
-              					<button type="button" class="close" data-dismiss="modal">&times;</button>	
-              				</div>
+            </div>
+            <div class="box-body" style="overflow-x:auto;">
+              <table id="example2" class="table table-bordered table-hover">
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Designation</th>
+                    <th>Designation Code</th>
+                    <th>Department Id</th>
+                    <th>CreateDate</th>
+                    <th>Status</th>
+                    <th>Update</th>
+                    <th>Delete</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php foreach ($designationdetails as $key) { ?>
+                  <tr>
+                    <td><?php echo $key->id;?></td>
+                    <td><?php echo $key->designation; ?></td> 
+                    <td><?php echo $key->designation_code; ?></td>
+                    <td><?php echo $key->department_code; ?></td> 
+                    <td><?php echo __date_format($key->created_at,'ddmmyyyy'); ?></td>  
+                    <td><?php echo $key->status; ?></td>  
+                    <td><a href="updatedesignation?id=<?php echo $key->designation_code;?>"><button type="button" class="btn btn-block btn-primary">UPDATE</button></a></td>
+                    <td><a href="deletedesignation?id=<?php echo $key->designation_code;?>"><button type="button" class="btn btn-block btn-primary">DELETE</button></a></td>          
+                  </tr> 
+                  <?php } ?>         
+                </tbody>
+              </table>
+            </div>
+          	<!-- Modal Start  -->
+            	<div class="modal fade" id="mymodel">
+            		<div class="modal-dialog modal-dialog-centered">
+            			<div class="modal-content">
+            				<div class="modal-header">
+            					<h3 class="text-center text-primary">Role</h3>	
+            					<button type="button" class="close" data-dismiss="modal">&times;</button>	
+            				</div>
+                    <!-- Form Start  -->
+                    <form action="<?php base_url('Admin/designationinsert');?>" id="formone" class="form-group" method="post" enctype="multipart/form-data">   
               				<div class="modal-body">
-                            <div class="row">
-  							              <div class="col-sm-12 col-md-4 col-lg-4">
-  							                <label>Designation Name</label>
-  							                <input type="text" class="form-control" placeholder="Enter Designation Name" name="DesignationName">
-  							              </div>
-  							              <div class="col-sm-12 col-md-4 col-lg-4">
-  							              	<label>Department Name</label>
-  							                <select class="form-control" name="designationtcombo">
-													<?php foreach($depts as $row)
-															{
-													?>
-													<option value="<?php echo $row->department_code;?>"><?php echo $row->department;?></option>
-													<?php
-														}
-													?>
-												</select>
-  							              </div>
-  							              
-  							              <div class="col-sm-12 col-md-12 col-lg-4">
-  							                <label>ClientId</label>
-  							                <input type="text" class="form-control" name="ClientId" value="<?php echo $this->session->userdata('org_code');?>" disabled>
-  							              </div>
-  							            </div>
-							       
+                        <div class="row">
+  							          <div class="col-sm-12 col-md-4 col-lg-4">
+  							            <label>Designation Name</label>
+  							            <input type="text" class="form-control" placeholder="Enter Designation Name" name="DesignationName">
+  							          </div>
+  							          <div class="col-sm-12 col-md-4 col-lg-4">
+    							          <label>Department Name</label>
+    							          <select class="form-control" name="designationtcombo">
+  													  <?php foreach($depts as $row) { ?>
+  													    <option value="<?php echo $row->department_code;?>"><?php echo $row->department;?></option>
+  													  <?php } ?>
+  												  </select>
+    							        </div>
+                          <div class="col-sm-12 col-md-12 col-lg-4">
+    							          <label>ClientId</label>
+    							          <input type="text" class="form-control" name="ClientId" value="<?php echo $this->session->userdata('org_code');?>" disabled>
+    							        </div>
+  							        </div>
               				</div>
               				<div class="modal-footer">
               					<div class="row">
 							              <div class="col-md-12 text-center" style="margin-top: 10px;">
-							               <button type="submit" class="btn btn-primary">Submit</button>
+							                <button type="submit" class="btn btn-primary">Submit</button>
 							              </div>
 							         </div>
               				</div>
-              			</div>
+                    </form>
+                    <!-- Form End -->
               		</div>
               	</div>
+              </div>
             	<!-- End Modal  -->
-        </form>
-         <!-- End Form Start  -->
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body" style="overflow-x:auto;">
-              <table id="example2" class="table table-bordered table-hover">
-                <thead>
-                <tr>
-                    <th>ID</th>
-					<th>Designation Name</th>
-					<th>Designation Code</th>
-					<th>Department Id</th>
-					<th>organization_code</th>
-					<th>Branch Code</th>
-					<th>CreateDate</th>
-					<th>Status</th>
-					<th>IpAddress</th>
-					<th>Update</th>
-					<th>Delete</th>
-			    </tr>
-                </thead>
-               <tbody>
-              <?php
-	foreach ($designationdetails as $key) {
-	?>
-	<tr>
-		<td><?php echo $key->id;?></td>
-		<td><?php echo $key->designation; ?></td>	
-		<td><?php echo $key->designation_code; ?></td>
-		<td><?php echo $key->department_code; ?></td>	
-		<td><?php echo $key->org_code;?></td>	
-		<td><?php echo $key->branch_code; ?></td>	
-		<td><?php echo $key->created_at; ?></td>	
-		<td><?php echo $key->status; ?></td>	
-		<td><?php echo $key->ip_address; ?></td>	
-		<td><a href="updatedesignation?id=<?php echo $key->designation_code;?>"><button type="button" class="btn btn-block btn-primary">UPDATE</button></a></td>
-		<td><a href="deletedesignation?id=<?php echo $key->designation_code;?>"><button type="button" class="btn btn-block btn-primary">DELETE</button></a></td>					
-	</tr>	
-	<?php
-	}
-		?>
-                
-                </tbody>
-                <tfoot>
-                <tr>
-                    <th>ID</th>
-					<th>Designation Name</th>
-					<th>Designation Code</th>
-					<th>Department Id</th>
-					<th>organization_code</th>
-					<th>Branch Code</th>
-					<th>CreateDate</th>
-					<th>Status</th>
-					<th>IpAddress</th>
-					<th>Update</th>
-					<th>Delete</th>
-			    </tr>
-                </tfoot>
-              </table>
-            </div>
-            <!-- /.box-body -->
           </div>
           <!-- /.box -->
         </div>
@@ -138,8 +90,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
       <!-- /.row -->
     </section>
     <!-- /.content -->
-
-  <!-- /.content-wrapper -->
 
 <!-- page script -->
 <script>
