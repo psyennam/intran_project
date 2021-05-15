8193e4d8a4068805314ba975805422531e6b285d
vishaly99 <81356194+vishaly99@users.noreply.github.com>
2021-05-15 16:57:19 +0530

    Updated layout

--- application/views/admin/components/layout.php
+++ application/views/admin/components/layout.php
@@ -64,6 +64,50 @@
   ?>
 <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
+    <section class="content">
+      <div class="row">
+        <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="info-box">
+              <span class="info-box-icon bg-aqua"><i class="ion ion-ios-gear-outline"></i></span>
+              <div class="info-box-content">
+                <span class="info-box-text">TOTAL VISIT</span>
+                <span class="info-box-number">90<small>%</small></span>
+              </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-red"><i class="fa fa-google-plus"></i></span>
+            <div class="info-box-content">
+              <span class="info-box-text">TODAY VISIT</span>
+              <span class="info-box-number">41,410</span>
+            </div>
+          </div>
+        </div>
+        <div class="clearfix visible-sm-block"></div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-green"><i class="ion ion-ios-cart-outline"></i></span>
+            <div class="info-box-content">
+              <span class="info-box-text">Sales</span>
+              <span class="info-box-number">760</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-yellow"><i class="ion ion-ios-people-outline"></i></span>
+            <div class="info-box-content">
+              <span class="info-box-text">TOTAL EXPENSE</span>
+              <span class="info-box-number">2,000</span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+        </div>
+      </div>
+
+    </section>
+
     <?php if(!empty($page)){
           $this->load->view($page);
         } ?>
